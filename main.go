b897970c21f9d69e8fdd6e93f6a222da0515f0c9
justyn temme <justntemme@gmail.com>
2016-09-09 14:49:33 -0500

    more bare bones work

--- main.go
+++ main.go
@@ -2,9 +2,11 @@ package main
 
 import (
 	"github.com/justyntemme/biteMyRSS/rss"
+	"github.com/justyntemme/biteMyRSS/web"
 )
 
 func main() {
 	rss.LoadRSS()
+	web.StartServer()
 
 }
--- web/web.go
+++ web/web.go
@@ -1,6 +1,37 @@
 package web
 
-//StartServer exported to start web server at beggining on program start
-func StartServer() {
+import (
+	"html/template"
+	"net/http"
+)
 
+//Page exported stuct which holds page information to be rendered
+type Page struct {
+	Title string
+	Body  string
+	Type  string
+}
+
+//StartServer exported to start web server at beggining on program start
+func StartServer(addr string) {
+	fs := http.FileServer(http.Dir("static"))
+	http.Handle("/static", http.StripPrefix("/static", fs))
+	http.HandleFunc("/", serveHTTP)
+	http.ListenAndServe(addr, nil)
+
+}
+
+func serveHTTP(d http.ResponseWriter, req *http.Request) {
+	serveTemplate(d, &Page{Title: "biteMyRSS", Body: "", Type: "home"})
+
+}
+
+func serveTemplate(d http.ResponseWriter, page *Page) {
+	d.Header().Add("Content Type", "text/html")
+	var file string
+	if page.Type == "home" {
+		file = "home"
+	}
+	tmpl, _ := template.ParseFiles("templates/home.html")
+	tmpl.ExecuteTemplate(d, file, page)
 }
